d652afed6dc4acbdde3e374093f933beda0c021c
Koichiro Nishijima <koichiro.nishijima@gmail.com>
2014-08-16 11:28:53 +0900

    Create gps_sample.ino

---
+++ gps_sample.ino
@@ -0,0 +1,62 @@
+#include <SoftwareSerial.h>
+#include <WString.h>                 // Stringライブラリ
+#define maxLength 30
+
+
+// GPS Setup
+#define rxGPS 3
+#define txGPS 5
+SoftwareSerial serialGPS = SoftwareSerial(rxGPS, txGPS);
+String stringGPS = "";
+
+void setup() {
+  pinMode(rxGPS, INPUT);
+  pinMode(txGPS, OUTPUT);
+
+
+  Serial.begin(9600);
+  Serial.println("Started");
+
+  // GPS Setup
+  serialGPS.begin(4800);
+  digitalWrite(txGPS,HIGH);
+
+  // Cut first gibberish
+  while(serialGPS.available())
+    if (serialGPS.read() == '\r')
+      break;
+}
+
+void loop()
+{
+  String s = checkGPS();
+  if(s && s.substring(0, 6) == "$GPGGA")
+  {
+    Serial.println(s);
+  }
+}
+
+// Check GPS and returns string if full line recorded, else false
+String checkGPS()
+{
+  if (serialGPS.available())
+  {
+    char c = serialGPS.read();
+    if (c != '\n' && c != '\r')
+    {
+      stringGPS = String(stringGPS + c);
+    }
+    else
+    {
+      if (stringGPS != "")
+      {
+        String tmp = stringGPS;
+        stringGPS = "";
+        return tmp;
+      }
+    }
+  } else {
+      Serial.println("GPS not available...");
+  }
+  return false;
+}
